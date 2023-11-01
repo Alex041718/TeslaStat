042008fc6577a2b8f7761f8f6a36b20923d5973c
Alexandre <Alexandre.alm18@gmail.com>
2023-11-01 08:45:57 +0100

    fzefez

--- addEnter.js
+++ addEnter.js
@@ -1,23 +1,31 @@
 import { readFile, writeFile } from 'fs';
 
 export const addPrice = (price) => {
-  // Lire le fichier JSON existant
+  const newData = { price, date: Date.now() };
+
+  // Lire le fichier JSON existant (ou le créer s'il n'existe pas)
   readFile('data.json', 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
+    let jsonData = [];
+    if (!err) {
+      // Le fichier JSON existe, donc analyser son contenu
+      try {
+        jsonData = JSON.parse(data);
+        if (!Array.isArray(jsonData)) {
+          throw new Error('Le fichier JSON n\'est pas un tableau valide.');
+        }
+      } catch (parseError) {
+        console.error('Erreur d\'analyse du fichier JSON existant:', parseError);
+        return;
+      }
     }
 
-    // Analyser le fichier JSON en un tableau d'objets
-    const jsonData = JSON.parse(data);
-
     // Ajouter les nouvelles données au tableau existant
-    jsonData.push({ price, date: Date.now() });
+    jsonData.push(newData);
 
-    // Réécrire le fichier JSON avec les nouvelles données
-    writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => {
-      if (err) {
-        console.error(err);
+    // Réécrire le fichier JSON avec les données mises à jour
+    writeFile('data.json', JSON.stringify(jsonData, null, 2), (writeError) => {
+      if (writeError) {
+        console.error('Erreur d\'écriture dans le fichier JSON :', writeError);
         return;
       }
       console.log('Données ajoutées au fichier JSON avec succès.');
